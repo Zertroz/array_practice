fb96b25e449486c98bc19df85211a73c324d580a
Kass Daniello <zertroz@gmail.com>
2022-11-09 17:53:47 -0500

    Add methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,16 @@
 var names = ["Kass", "V", "Valerie", "Ward"];
 var ages = [16, 25, 43, 67];
-var boolean = [true, false, false, true, true]
+var boolean = [true, false, false, true]
+
+// This method will replace all elements in an array with the inputted value.
+names.fill("Kass");
+console.log(names);
+
+// This method will remove the first element from the array, which can then be returned by logging a new variable.
+var firstAge = ages.shift();
+console.log(ages);
+console.log(firstAge);
+
+// This method will add a new element to the array.
+boolean.push(false)
+console.log(boolean)
